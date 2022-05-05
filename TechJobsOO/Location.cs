d98cc46cd3d3a3cf8d2c4e47b6304fd1efdeee61
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-05-05 16:28:25 -0400

    Location constructor added

--- TechJobsOO/Location.cs
+++ TechJobsOO/Location.cs
@@ -13,6 +13,11 @@ namespace TechJobsOO
             nextId++;
         }
 
+        public Location (string value) : this()
+        {
+            Value = value;
+        }
+
         // TODO: Add a second constructor to this class that uses the Location() constructor and sets the value of the value field.
 
         public override bool Equals(object obj)
