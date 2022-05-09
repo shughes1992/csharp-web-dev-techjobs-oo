e1bc68dc9e06f1aa70e077f7bd3eb7fd3987b79b
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-05-09 09:47:58 -0400

    updates PositionType class & addes unitTest proj

---
+++ JobTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JobTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
